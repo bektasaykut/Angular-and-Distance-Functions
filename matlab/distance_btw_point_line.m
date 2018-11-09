aac1e6705b285c4d98edaaed8065bde5b8ed48d7
bektasaykut <bektasaykut@gmail.com>
2018-11-09 11:25:30 +0300

    Create distance_btw_point_line.m

---
+++ matlab/distance_btw_point_line.m
@@ -0,0 +1,24 @@
+function [x,y,distance] = distance_btw_point_line(point, p1, p2)
+%(
+[x,y,distance] = distance_point_btw_line([0,0],[0,-4],[-4,0])
+x = -2
+y = -2
+distance =  2.8284
+%)
+    x1 = p1(1); y1 = p1(2);
+    x2 = p2(1); y2 = p2(2);
+    xp = point(1); yp = point(2);
+    
+    Mo = (y2-y1)/(x2-x1); %(slope of line between p1 to p2)
+    Mn = -Mo;
+    
+    K1=Mo*x1-y1;
+    
+    K2=Mn*xp-yp;
+    
+    x = (K1-K2)/(2*Mo);
+    
+    y = Mo*(-K1-K2)/(2*Mo);
+    
+    distance = sqrt((xp-x)^2+(yp-y)^2);    
+end 
