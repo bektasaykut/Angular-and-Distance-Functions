bc3dd4cac14e2da59aa5f184111e892ade98e9e5
bektasaykut <bektasaykut@gmail.com>
2018-11-09 11:27:08 +0300

    Update distance_btw_point_line.m

--- matlab/distance_btw_point_line.m
+++ matlab/distance_btw_point_line.m
@@ -1,10 +1,11 @@
 function [x,y,distance] = distance_btw_point_line(point, p1, p2)
-%(
-[x,y,distance] = distance_point_btw_line([0,0],[0,-4],[-4,0])
-x = -2
-y = -2
-distance =  2.8284
-%)
+%
+%[x,y,distance] = distance_point_btw_line([0,0],[0,-4],[-4,0])
+%Results:
+%x = -2
+%y = -2
+%distance =  2.8284
+
     x1 = p1(1); y1 = p1(2);
     x2 = p2(1); y2 = p2(2);
     xp = point(1); yp = point(2);
